70af15159f22fcdedc9419210a4b892a1d573e9f
David Grammatico <david.lugdunum@gmail.com>
2023-12-01 20:56:06 +0100

    Add @radix-ui/react-popover dependency and update profil.tsx

--- components/nav/profil.tsx
+++ components/nav/profil.tsx
@@ -1,17 +1,60 @@
 'use client'
 
 import Image from 'next/image'
+import Link from 'next/link'
+import { MdOutlineDashboard } from 'react-icons/md'
+import { RiLogoutCircleLine } from 'react-icons/ri'
 import { useUser } from '@/lib/store/user'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { Button } from '@/components/ui/button'
+import { supabase } from '@/lib/supabase'
 
 export default function Profil() {
   const user = useUser((state) => state.user)
+  const setUser = useUser((state) => state.setUser)
+
+  const handleLogout = () => {
+    supabase.auth.signOut()
+    setUser(undefined)
+  }
   return (
-    <Image
-      width={64}
-      height={64}
-      src={user?.user_metadata.avatar_url}
-      alt={user?.user_metadata.user_name}
-      className="rounded-full ring-2 ring-green-500"
-    />
+    <Popover>
+      <PopoverTrigger>
+        <Image
+          width={64}
+          height={64}
+          src={user?.user_metadata.avatar_url}
+          alt={user?.user_metadata.user_name}
+          className="rounded-full ring-2 ring-green-500"
+        />
+      </PopoverTrigger>
+      <PopoverContent className="p-2 space-y-3 divide-y">
+        <div className="px-4 text-sm">
+          <p>{user?.user_metadata?.user_name}</p>
+          <p className="text-gray-500">{user?.user_metadata?.email}</p>
+        </div>
+        <Link className="block" href="/dashboard">
+          <Button
+            className="w-full flex items-center justify-between"
+            variant="ghost"
+          >
+            Dashboard
+            <MdOutlineDashboard />
+          </Button>
+        </Link>
+        <Button
+          onClick={handleLogout}
+          className="w-full flex items-center justify-between"
+          variant="ghost"
+        >
+          Logout
+          <RiLogoutCircleLine />
+        </Button>
+      </PopoverContent>
+    </Popover>
   )
 }
