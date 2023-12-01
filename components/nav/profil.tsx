513bce514e9ca0d78446768bb9df886deb8bc2a5
David Grammatico <david.lugdunum@gmail.com>
2023-12-01 20:32:50 +0100

    update profil with image

--- components/nav/profil.tsx
+++ components/nav/profil.tsx
@@ -1,9 +1,17 @@
 'use client'
 
+import Image from 'next/image'
 import { useUser } from '@/lib/store/user'
 
 export default function Profil() {
   const user = useUser((state) => state.user)
-  console.log(user)
-  return <div>profil</div>
+  return (
+    <Image
+      width={64}
+      height={64}
+      src={user?.user_metadata.avatar_url}
+      alt={user?.user_metadata.user_name}
+      className="rounded-full ring-2 ring-green-500"
+    />
+  )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
