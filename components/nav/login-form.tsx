835761b0af27c5d56def42fd0bd2183437d2f860
David Grammatico <david.lugdunum@gmail.com>
2023-12-01 20:58:13 +0100

    Refactor login-form component

--- components/nav/login-form.tsx
+++ components/nav/login-form.tsx
@@ -1,16 +1,12 @@
 'use client'
 
 import { SiGithub } from 'react-icons/si'
-import { createBrowserClient } from '@supabase/ssr'
 import { usePathname } from 'next/navigation'
 import { Button } from '@/components/ui/button'
+import { supabase } from '@/lib/supabase'
 
 export default function LoginForm() {
   const pathname = usePathname()
-  const supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  )
 
   const handleLogin = async () => {
     await supabase.auth.signInWithOAuth({
